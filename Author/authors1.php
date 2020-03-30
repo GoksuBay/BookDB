ab439ee21384d5afdeca757fc757754f39c655d7
Ceren <48736578+cerenkeklik@users.noreply.github.com>
2020-03-30 14:45:29 +0300

    author

---
+++ Author/authors1.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Author1</title>
+<meta charset="utf-8">
+
+
+</head>
+<body>
+
+<style>
+
+header{
+padding: 20px;
+background-color:coral;
+text-align: center;
+font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif ;
+font-size: 25px;
+
+
+}
+
+nav{
+float: left;
+padding: 20px;
+text-align: center;
+font-size: 20px;
+width:300px;
+height:400px;
+}
+
+article{
+float: right;
+padding: 20px;
+padding-top: 30px;
+text-align: left;
+font-size: 18px;
+width:950px;
+height:400px;
+
+
+}
+
+.books{
+    padding-left: 60px;
+}
+
+</style>
+
+<header>Author1</header>
+
+<nav>
+
+<img src="authorpic.jpg" width="200" height="200">
+<p>puan</p>
+</nav>
+
+<article>
+<h3>About</h3>
+<p> Cumhuriyet döneminin önemli romancılarından olan Orhan Kemal, 15 Eylül 1914 tarihinde Adana’da dünyaya geldi. 
+    Adana’nın Fransızlar tarafından işgal edilmesi üzerine ailesiyle birlikte bir süre Niğde ve Konya’da kaldı.
+    1931 yılında öğrenimini yarıda bırakarak ailesiyle birlikte bu kez Beyrut’a gitti.
+    Bir yıl burada kaldıktan sonra Adana’ya dönerek babaannesinin yanına yerleşti. 
+    Bir süre çeşitli işlerde çalıştıktan sonra askere gitti.
+    Askerlik görevi sırasında “Nazım Hikmet ve Maksim Gorki’nin kitaplarını okumak” ve “Yabancı rejimler lehine propaganda” suçlaması ile karşı karşıya kaldı.
+    Yargılama sonucunda 5 yıl hapis cezasına çarptırıldı.
+    “Duvarlar” başlıklı ilk şiiri, 1939 yılında Yedigün adlı dergide yayımlandı.
+    Cezaevinde Nazım Hikmet ile tanışan Orhan Kemal, onun teşvikleri sonucunda roman ve öykü yazmaya yöneldi.
+    Cezaevinden çıktıktan sonra bir süre hamal olarak çalıştı.
+    1948 yılında ilk öykü kitabı “Duygu“, ertesi yıl da ilk romanı “Baba Evi” yayımlandı.
+    1958 yılında “Kardeş Payı” öyküsü ile Sait Faik Hikaye Armağanı‘nı kazandı. 1969 yılında “Önce Ekmek” adlı eseri ile tekrardan Sait Faik Hikaye Armağanı‘nı ve Türk Dil Kurumu Ödülü‘nü kazandı.
+    Orhan Kemal, 2 Haziran 1970 tarihinde Sofya’da hayatını kaybetti.
+</p>
+
+
+</article>
+
+<books>
+<h3>Books Top 6</h3>
+<img src="authorsbookpic.jpg" width="210" height="300">
+<img src="authorsbookpic.jpg" width="210" height="300">
+<img src="authorsbookpic.jpg" width="210" height="300">
+<img src="authorsbookpic.jpg" width="210" height="300">
+<img src="authorsbookpic.jpg" width="210" height="300">
+<img src="authorsbookpic.jpg" width="210" height="300">
+</books>
+
+<footer>
+<h3>All Books</h3>
+<ul style="list-style-type:none;">
+
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+
+
+
+</ul>
+
+
+
+
+
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ Author/authorsMain.php
@@ -0,0 +1,172 @@
+<?php
+
+$db = mysqli_connect("localhost", "moderator", "moderator", "books");
+
+if($db->connect_error)
+ die("connection failed :" . $db->connect_error);
+ else
+ echo "connected succesfully";
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Author1</title>
+<meta charset="utf-8">
+
+</head>
+<body>
+
+<style>
+
+header{
+padding: 20px;
+background-color:coral;
+text-align: center;
+font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif ;
+font-size: 25px;
+
+} 
+
+nav ul{
+float: left;
+width:400px;
+height:720px;
+padding: 20px;
+background-color:cornflowerblue;
+text-align: center;
+font-size: 20px;
+
+}
+
+</style>
+<header>Authors</header>
+<nav>
+<ul style="list-style-type:none;">
+
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+<li><a href="author1.html">author1</a></li>
+
+
+</ul>
+</nav>
+
+<nav>
+
+<ul style="list-style-type:none;">
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+<li><a href="author1.html">author2</a></li>
+
+
+</ul>
+
+
+
+
+</nav>
+
+<nav>
+
+<ul style="list-style-type:none;">
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+<li><a href="author1.html">author3</a></li>
+
+</ul>
+
+
+
+
+</nav>
+</body>
+</html>
