2daae61d3f2189c08b2c4e0bda5edee2d0bc1ac9
Ceren <48736578+cerenkeklik@users.noreply.github.com>
2020-04-30 04:39:51 +0300

    categories

---
+++ Project/Categories/categories.php
@@ -0,0 +1,47 @@
+<?php
+session_start(); ?>
+<!DOCTYPE html
+<head> 
+  <h1 style="text-align: center">Categories</h1>
+</head>
+</html>
+<?php
+require "../includes/dbconnect.php";
+
+$categoriesSql="SELECT * FROM category";
+$categoriesResult=mysqli_query($connect,$categoriesSql);
+while($pullCate=mysqli_fetch_assoc($categoriesResult)){
+  ?>   
+  <li style="list-style-type:none">
+  <img src=  "<?php echo '../Admin/'  .$pullCate['photo'];  ?> " style="width:200px;height:200px;"> </img> 
+
+<a href ="categories1.php?id=<?php echo $pullCate['id']?>"  > <li style="list-style-type: none"><?php print_r($pullCate['categoryName']); } ?></li> 
+</li> 
+
+
+<!DOCTYPE html>
+
+<head>
+
+</head>
+<body>
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+</body>
+
+
+</html>
---
+++ Project/Categories/categories1.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$id = intval($_GET['id']);
+require "../includes/dbconnect.php";
+$categoriesSql="SELECT * FROM category WHERE id='$id'";
+$categoriesResult=mysqli_query($connect,$categoriesSql);
+$pullCate=mysqli_fetch_assoc($categoriesResult);
+
+?>
+<!DOCTYPE html
+<head>
+<h1 style="text-align: center"> <?php echo $pullCate['categoryName']; ?>  </h1>
+</head>
+</html>
+<?php
+
+$id = intval($_GET['id']);
+$categorysql="SELECT * FROM book WHERE categoryID='$id'";
+$categoryResult=mysqli_query($connect,$categorysql);
+while($pullBook=mysqli_fetch_assoc($categoryResult)){
+?>
+
+
+<li style="list-style-type: none"> <a href = "../Bookoperations/bookInfo.php?ISBN=<?php echo $pullBook['ISBN']?>" > <?php echo $pullBook['name']; } ?> </a> </li>
+
+
+
+
+
